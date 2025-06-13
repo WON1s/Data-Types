53c28cf65701814ea2de7abadf97be91db44cc00
WON1s <mudathiradan@gmail.com>
2025-06-13 11:19:53 -0700

    Add files via upload
    initial commit

---
+++ Data Types.py
@@ -0,0 +1,27 @@
+#integer
+age = 11
+print(age)
+print (type(age))
+
+# string
+name="Mudathir Adan"
+print(name)
+print(type(name))
+
+# float
+height=5.2
+print(height)
+print(type(height))
+
+# typecastiing
+height=int(height)
+print(height)
+print(type(height))
+
+age=str(age)
+print(age)
+print(type(age))
+
+# user input
+weight=input("what is your weight?: ")
+print(weight)
